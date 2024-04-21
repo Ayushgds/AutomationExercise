6b5c5ef63ef028b1684d02a8527aa0b10151337f
Ayushgds <164007777+Ayushgds@users.noreply.github.com>
2024-04-21 13:17:07 +0100

    Add files via upload

---
+++ src/main/java/Exe4.java
@@ -0,0 +1,10 @@
+public class Exe4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (b>a)
+            System.out.println("20 greater than 10");
+        else
+            System.out.println("lol");
+    }
+}
